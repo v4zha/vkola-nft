3d2a3d583e9baf77cb2d11e7347f794af27f4fae
v4zha <arvin_j4848@protonmail.com>
2022-03-26 17:56:09 +0530

    meta

--- programs/vnft/src/lib.rs
+++ programs/vnft/src/lib.rs
@@ -1,6 +1,9 @@
-
-use anchor_lang::{prelude::*};
-use mpl_token_metadata::{instruction::create_metadata_accounts_v2, state::{Collection,Creator,Uses, UseMethod}};
+use anchor_lang::prelude::*;
+use mpl_token_metadata::{
+    instruction::create_metadata_accounts_v2,
+    state::{Collection, Creator, UseMethod, Uses},
+};
+use solana_program::program::invoke;
 
 declare_id!("AHqPK6YeALn9xFEVqUvN41vsRxrWypWoWHjng1jSgZDj");
 
@@ -8,11 +11,19 @@ declare_id!("AHqPK6YeALn9xFEVqUvN41vsRxrWypWoWHjng1jSgZDj");
 pub mod vnft {
 
     use super::*;
-     pub fn mint_nft(ctx: Context<MintNft>,token:Pubkey,name:String,symbol:String,uri:String,creator:String,collection:String) -> Result<()> {
-        let account=&mut ctx.accounts.mint_id;
-        account.nft_token=token;
-        account.owner=ctx.accounts.authority.key();
-        let nft_meta=NtfMeta::new(token,account.owner,name,symbol,uri);
+    pub fn mint_nft(
+        ctx: Context<MintNft>,
+        token: Pubkey,
+        name: String,
+        symbol: String,
+        uri: String
+    ) -> Result<()> {
+        let account = &mut ctx.accounts.mint_id;
+        let account_info =account.to_account_infos();
+        let meta_program=&mut ctx.accounts.meta_program;
+        account.nft_token = token;
+        account.owner = ctx.accounts.authority.key();
+        let nft_meta = NtfMeta::new(meta_program,account_info, token, account.owner, name, symbol, uri);
         nft_meta.create_meta();
         Ok(())
     }
@@ -25,16 +36,18 @@ pub mod vnft {
 
 #[derive(Accounts)]
 pub struct MintNft<'info> {
-#[account(init,payer=authority,space=64)]
-pub mint_id:Account<'info,VazhaNft>,
-#[account(mut)]
-pub authority:Signer<'info>,
-pub system_program:Program<'info,System>,
+    #[account(init,payer=authority,space=64)]
+    pub mint_id: Account<'info, VazhaNft>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    #[account(address = mpl_token_metadata::id())]
+    meta_program: UncheckedAccount<'info>,
+    pub system_program: Program<'info, System>,
 }
 #[account]
-pub struct VazhaNft{
-pub nft_token:Pubkey,
-pub owner:Pubkey,
+pub struct VazhaNft {
+    pub nft_token: Pubkey,
+    pub owner: Pubkey,
 }
 // #[derive(Accounts)]
 // pub struct TransferNft<'info>{
@@ -43,38 +56,75 @@ pub owner:Pubkey,
 //     pub owner:Signer<'info>
 // }
 
-struct NtfMeta{
-pid:Pubkey,
-account:Pubkey,
-mint:Pubkey,
-owner:Pubkey,
-name:String,
-symbol:String,
-uri:String,
-creator:Option<Vec<Creator>>,
-collection:Option<Collection>,
-seller_fee_basis_points:u16,
-is_mutable:bool,
-uses:Option<Uses>,
+struct NtfMeta<'info> {
+    meta_program:UncheckedAccount<'info>,
+    account_info:Vec<AccountInfo<'info>>,
+    token: Pubkey,
+    owner: Pubkey,
+    name: String,
+    symbol: String,
+    uri: String,
+    creators: Option<Vec<Creator>>,
+    collection: Option<Collection>,
+    seller_fee_basis_points: u16,
+    is_mutable: bool,
+    uses: Option<Uses>,
 }
-impl NtfMeta{
-    fn new(mint:Pubkey,owner:Pubkey,name:String,symbol:String,uri:String)->Self{
-       Self{
-           pid:Pubkey::new(b"metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"),
-            account:owner,
-            mint,
+impl<'info> NtfMeta<'info> {
+    fn new(
+        meta_program:UncheckedAccount<'info>,
+        account_info:Vec<AccountInfo<'info>>,
+        token: Pubkey,
+        owner: Pubkey,
+        name: String,
+        symbol: String,
+        uri: String,
+    ) -> Self {
+        Self {
+            meta_program,
+            account_info,
+            token,
             owner,
             name,
             symbol,
             uri,
-            creator:Some(vec![Creator{address:owner,verified:true,share:100}]),
-            collection:Some(Collection{key:owner,verified:true}),
-            seller_fee_basis_points:12,
-            is_mutable:false,
-            uses:Some(Uses{use_method:UseMethod::Single,remaining:1,total:1})
-       } 
+            creators: Some(vec![Creator {
+                address: owner,
+                verified: true,
+                share: 100,
+            }]),
+            collection: Some(Collection {
+                key: owner,
+                verified: true,
+            }),
+            seller_fee_basis_points: 12,
+            is_mutable: false,
+            uses: Some(Uses {
+                use_method: UseMethod::Single,
+                remaining: 1,
+                total: 1,
+            }),
+        }
     }
-    fn create_meta(self){
-        // let instructions=create_metadata_accounts_v2(program_id, metadata_account, mint, mint_authority, payer, update_authority, name, symbol, uri, creators, seller_fee_basis_points, update_authority_is_signer, is_mutable, collection, uses);
+    fn create_meta(self)->Result<()> {
+        let instructions = create_metadata_accounts_v2(
+            self.meta_program.key(),
+            self.token,
+            self.token,
+            self.owner,
+            self.owner,
+            self.owner,
+            self.name,
+            self.symbol,
+            self.uri,
+            self.creators,
+            self.seller_fee_basis_points,
+            true,
+            self.is_mutable,
+            self.collection,
+            self.uses,
+        );
+        invoke(&instructions, &self.account_info).unwrap();
+        Ok(())
     }
 }
